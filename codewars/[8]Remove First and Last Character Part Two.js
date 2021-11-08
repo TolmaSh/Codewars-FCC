21243d25d4d15177e3ec35067663bd29b7477535
TolmaSh <m0v4anua@gmail.com>
2021-11-08 14:18:41 +0200

    fix: Remove First and Last Character Part Two

--- codewars/[8]Remove First and Last Character Part Two.js
+++ codewars/[8]Remove First and Last Character Part Two.js
@@ -1,15 +1,7 @@
 function array(arr){
-  if ( arr.length > 3) {
-     arr = arr.replace(/[^0-9]/g, '');
-     arr = arr.split("");
-     arr = String(arr);
-     arr = arr.substring(2, arr.length-2);
-     arr = arr.replace(/[^0-9]/g, ' ');
- 
-    return arr
-  } else {
-    return null
-  }
- 
+  const characters = arr.split(',')
+  characters.shift()
+  characters.pop()
+  return characters.length ? characters.join(' ') : null
  }
  
