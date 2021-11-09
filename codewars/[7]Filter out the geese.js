2f9a18a1609d3c7b485dcac04b75356ea5e0eff9
TolmaSh <m0v4anua@gmail.com>
2021-11-09 10:10:08 +0200

    feat: Filter out the geese

---
+++ codewars/[7]Filter out the geese.js
@@ -0,0 +1,7 @@
+function gooseFilter (birds) {
+  var geese = ["African", "Roman Tufted", "Toulouse", "Pilgrim", "Steinbacher"];
+  return birds.filter(item => 
+    !geese.includes(item)
+  )
+  // return an array containing all of the strings in the input array except those that match strings in geese
+};
