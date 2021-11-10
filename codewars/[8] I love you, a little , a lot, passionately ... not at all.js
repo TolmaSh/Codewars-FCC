ed58ee6ce0b41789e1719e6ab6df4dd6579f3b56
TolmaSh <m0v4anua@gmail.com>
2021-11-10 12:56:03 +0200

    feat: I love you, a little , a lot, passionately ... not at all

---
+++ codewars/[8] I love you, a little , a lot, passionately ... not at all.js
@@ -0,0 +1,16 @@
+function howMuchILoveYou(nbPetals) {
+  const arr = [
+     "I love you",
+     "a little",
+     "a lot",
+     "passionately",
+     "madly",
+     "not at all",
+   ];
+    
+  for (let i = 0; i <= nbPetals-1; i += 1) {
+    
+    if (i === nbPetals-1) {
+      return arr[i%6];
+    }
+  }
